f3e07525fc2e9a12b7b14965d42527509d6a2b8b
AbhinavSingh <abhinavgautam123456@gmail.com>
2025-11-06 19:38:28 +0545

    Button/Navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -32,11 +32,13 @@ export default function Navbar() {
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    className="feather feather-menu">
+                    className="feather feather-menu md:hidden">
                                 <line x1="3" y1="12" x2="21" y2="12"></line>
                                 <line x1="3" y1="6" x2="21" y2="6"></line>
                                 <line x1="3" y1="18" x2="21" y2="18"></line>
                                 </svg>
+                                <button className="border-white h-12 rounded-full px-6 font-medium">Log In</button>
+                                <button>Sign Up</button>
                 </div>
             </div>
         </div> 
